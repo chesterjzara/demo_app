1b4faddbf7298218aa47eeaa6fa4d0136f8f2c80
CJ Zara <chesterjzara@gmail.com>
2014-01-28 18:33:38 -0600

    Add welcome page for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 DemoApp::Application.routes.draw do
   resources :microposts
-
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
